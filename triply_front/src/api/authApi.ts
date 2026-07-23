10ac5ff2cdd1964f988af525168e2679cee13c53
suyeon1104 <55120700+suyeon1104@users.noreply.github.com>
2026-07-23 16:34:39 +0900

    마이페이지 사용자 데이터 받아와서 표시

--- triply_front/src/api/authApi.ts
+++ triply_front/src/api/authApi.ts
@@ -3,7 +3,7 @@ import axiosInstance from "./axiosInstance";
 
 export const join = (data) => axiosInstance.post("/auth/join", data);
 export const login = (data) => axiosInstance.post("/auth/login", data);
-export const getProfile = () => axiosInstance.get("/getProfile");
+export const getProfile = () => axiosInstance.get("/user/getProfile");
 export const patchProfile = (data) => axiosInstance.patch("/patchProfile", data);
 export const idCheck = (loginId) => axiosInstance.post("user/auth/idCheck", { loginId });
 export const phoneCheck = () => axiosInstance.get("/user/auth/phoneCheck");
--- triply_front/src/pages/MyPage.tsx
+++ triply_front/src/pages/MyPage.tsx
@@ -1,16 +1,49 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/useAuth";
+import { useEffect, useState } from "react";
+import { getProfile } from "../api/authApi";
+
+interface UserData {
+  loginId: string;
+  userName: string;
+  userPhone: string;
+  result: boolean;
+}
+
 
 export default function MyPage() {
   const navigate = useNavigate();
 
-    const {logout } = useAuth();
-    function handleLogout() {
-      logout();
-      console.log("navigate 실행 직전");
-      navigate("/login");
-      console.log("navigate 실행 직후");
-    }
+  const {logout } = useAuth();
+  const [copied, setCopied] = useState(false);
+  const [userData, setUserData] = useState<UserData | null>(null);
+  // const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProfile();
+      if (!res) return;
+      console.log(res.data);
+      setUserData(res.data);
+    })();
+  }, []);
+
+  // user mock data
+  // -> 실제로는 /getProfile API 응답으로 대체
+
+  // const user = {
+  //   name: "김수연",
+  //   loginId: "suyeon123",
+  //   phone: "010-1234-5678",
+  // };
+
+
+  function handleLogout() {
+    logout();
+    console.log("navigate 실행 직전");
+    navigate("/login");
+    console.log("navigate 실행 직후");
+  }
 
   return (
     <>
@@ -18,6 +51,11 @@ export default function MyPage() {
       <button onClick={handleLogout} aria-label="로그아웃">
         ❌
       </button>
+      <p>id: {userData?.loginId}</p>
+      <p>name: {userData?.userName}</p>
+      <p>phone: {userData?.userPhone}</p>
+      <p>result: {userData?.result}</p>
+
     </>
   );
 };
